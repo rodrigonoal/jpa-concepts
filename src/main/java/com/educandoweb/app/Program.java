ab0dfc421ccd848ffd41de2daa1da21418b1b6c3
Rodrigo Moraes Noal <rodrigonoal@gmail.com>
2022-03-17 17:03:41 -0300

    jpa create and delete

--- src/main/java/com/educandoweb/app/Program.java
+++ src/main/java/com/educandoweb/app/Program.java
@@ -12,34 +12,35 @@ public class Program {
         Person p2 = new Person(null, "Joaquim Torres", "joaquim@gmail.com");
         Person p3 = new Person(null, "Ana Maria", "ana@gmail.com");
 
-
         //agora criamos uma factory de entity managers e o entity manager à partir dele
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
 
-        //abrindo a conexão
+        //abrindo a conexão e transação
         em.getTransaction().begin();
         //salvando objetos no banco de dados:
         em.persist(p1);
         em.persist(p2);
         em.persist(p3);
-        //fechando a conexão
+        System.out.println("The objects have been saved!");
+        //fechando a transação
         em.getTransaction().commit();
+
+        //encontrando dado no banco
+        System.out.println("Searching...");
+        Person p = em.find(Person.class, 2);
+        System.out.println("Person found:" + p);
+
+        //deletando dado no banco
+        //rodar comentando tudo acima
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
+        System.out.println("Person 2 removed!");
+
+        //fechando a conexão
         em.close();
         emf.close();
 
-        System.out.println("The objects have been saved!");
     }
 }
-
-/*
-O output no database deverá ser o seguinte:
-        mysql> select * from Person;
-        +----+-------------------+-----------------+
-        | id | email             | nomecompleto    |
-        +----+-------------------+-----------------+
-        |  1 | carlos@gmail.com  | Carlos da Silva |
-        |  2 | joaquim@gmail.com | Joaquim Torres  |
-        |  3 | ana@gmail.com     | Ana Maria       |
-        +----+-------------------+-----------------+
-*/
