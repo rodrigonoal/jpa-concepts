904cbbe32e628c3b91f8fbc912ce1577981e8349
Rodrigo Moraes Noal <rodrigonoal@gmail.com>
2022-03-17 15:43:47 -0300

    connected with database & entity creation

--- src/main/java/com/educandoweb/app/Program.java
+++ src/main/java/com/educandoweb/app/Program.java
@@ -2,13 +2,42 @@ package com.educandoweb.app;
 
 import com.educandoweb.domain.Person;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Program {
     public static void main(String[] args){
-        Person p1 = new Person(1, "Carlos da Silva", "carlos@gmail.com");
-        Person p2 = new Person(2, "Joaquim Torres", "joaquim@gmail.com");
-        Person p3 = new Person(3, "Ana Maria", "ana@gmail.com");
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        Person p1 = new Person(null, "Carlos da Silva", "carlos@gmail.com");
+        Person p2 = new Person(null, "Joaquim Torres", "joaquim@gmail.com");
+        Person p3 = new Person(null, "Ana Maria", "ana@gmail.com");
+
+
+        //agora criamos uma factory de entity managers e o entity manager à partir dele
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        //abrindo a conexão
+        em.getTransaction().begin();
+        //salvando objetos no banco de dados:
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        //fechando a conexão
+        em.getTransaction().commit();
+
+        System.out.println("The objects have been saved!");
     }
 }
+
+/*
+O output no database deverá ser o seguinte:
+        mysql> select * from Person;
+        +----+-------------------+-----------------+
+        | id | email             | nomecompleto    |
+        +----+-------------------+-----------------+
+        |  1 | carlos@gmail.com  | Carlos da Silva |
+        |  2 | joaquim@gmail.com | Joaquim Torres  |
+        |  3 | ana@gmail.com     | Ana Maria       |
+        +----+-------------------+-----------------+
+*/
--- src/main/java/com/educandoweb/domain/Person.java
+++ src/main/java/com/educandoweb/domain/Person.java
@@ -1,10 +1,17 @@
 package com.educandoweb.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
 import java.io.Serializable;
 
 @Data
@@ -12,14 +19,14 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@Entity
+@Entity //essa anotação indica que esta classe é uma tabela!
 public class Person implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
-    @NonNull
+    @GeneratedValue //indicando que é um id automaticamente gerado
     private Integer id;
+    @Column(name = "nomecompleto") //renomeando a coluna da tabela
     @NonNull
     private String name;
     @NonNull
