a8da8f5f88656cd193cf77b6841d723c181024f0
Rodrigo Moraes Noal <rodrigonoal@gmail.com>
2022-03-17 14:12:17 -0300

    mapping & persistence config added

--- src/main/java/com/educandoweb/domain/Person.java
+++ src/main/java/com/educandoweb/domain/Person.java
@@ -2,16 +2,25 @@ package com.educandoweb.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Person implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @NonNull private Integer id;
-    @NonNull private String nome;
-    @NonNull private String email;
+    @Id
+    @GeneratedValue
+    @NonNull
+    private Integer id;
+    @NonNull
+    private String nome;
+    @NonNull
+    private String email;
 }
